fbe6745dde230c27b4a8df108cd6767d234499df
Jenifergs <contato.comercialjenifer@gmail.com>
2022-03-10 20:42:54 -0300

    Adiciona exercicios bloco 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/variaveis.js
@@ -0,0 +1,10 @@
+const myName="Jenifer";
+const birthCity="Serrinha-BA";
+birthCity="Salvador"
+let birthYear= 1994
+birthYear= 2030
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
