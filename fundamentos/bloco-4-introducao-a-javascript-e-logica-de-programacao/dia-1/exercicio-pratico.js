8b4d3104690f204f3c27a340474bfb22fda65f43
Jenifergs <contato.comercialjenifer@gmail.com>
2022-03-11 20:26:44 -0300

    Adiciona respostas do exercicio pratico 4.1 primeiros passos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/exercicio-pratico.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/exercicio-pratico.js
@@ -39,15 +39,107 @@
 
 // console.log(valorMaior);
 
-const number = 65;
-let elemento;
 
-if ( number > 0) {
-    elemento= positive;
-} 
-else if (number < 0) {
-    elemento= negative;
-} else {
-    elemento= zero;
-}
-console.log(elemento);
+// 4 - Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+// let numero= 0;
+// if(numero > 0) {
+//     console.log("positive");
+// } else if (numero < 0) {
+//     console.log("negative" );
+// } else {
+//     console.log("zero");
+// }
+
+// console.log(numero);
+
+// 5 - Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+// const anguloUm= -60;
+// const anguloDois= 60;
+// const anguloTres= 60;
+// if ( anguloUm < 0|| anguloDois < 0|| anguloTres < 0) {
+//     console.log("Este angulo é inválido");
+// }
+// if (anguloUm + anguloDois + anguloTres === 180) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 6 - Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+// let peca="Rei";
+// let pecaModificada= peca.toLowerCase();
+// switch (pecaModificada) {
+//     case "bispo": 
+//         console.log("diagonal");
+//         break;
+//     case "cavalo":
+//         console.log("em L");
+//         break;
+//     case "torre":
+//         console.log("em cruz");
+//         break;
+
+//     default: 
+//         console.log("peça invalida");
+//         break;
+// }
+
+// 7 - Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+// let nota= -60;
+// if (nota > 100 || nota < 0) {
+//     console.log("Erro");
+// }
+//     else if (nota >= 90) {
+//         console.log("A");
+//     } else if (nota >= 80) {
+//         console.log("B");
+//     } else if (nota >= 70) {
+//         console.log("C");
+//     } else if (nota >= 60) {
+//         console.log("D");
+//     } else if (nota >= 50 ) {
+//         console.log("E");
+//     } else {
+//         console.log("F");
+//     }
+
+// 8 - Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+// const n1 = 7;
+// const n2 = 6;
+// const n3 = 3;
+
+// if (n1 % 2 ===0 || n2 % 2 ===0 || n3 % 2 ===0 ){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 9 - Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+
+// const n1 = 74;
+// const n2 = 65;
+// const n3 = 32;
+// if (n1 % 2 ===1 || n2 % 2 ===1 || n3 % 2 ===1 ){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 10 - Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// const custo=7000.00
+// const venda=8000.00
+// if (venda < 0  || custo < 0){
+//     console.log("Erro: Valor de custo ou venda zerado");
+// } else { 
+// const imposto= 20/100;
+// const impostoSobreCusto = custo * imposto;
+// const custoTotal= custo + impostoSobreCusto;
+// const lucroTotal= venda - custoTotal;
+
+
+// console.log(lucroTotal * 1000);
+// }
+
+
+
+
