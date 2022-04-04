36ddebfac02a17537baa94d42bec5232c7212565
jenifergs <contato.comercialjenifer@gmail.com>
2022-04-04 15:39:33 -0300

    Adiciona respostas do bloco 7, dia 1

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -0,0 +1,30 @@
+//parte 1
+const testingScope = escopo=> {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo ! `; 
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
+  testingScope(true);
+
+  //parte 2
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
+}
+
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
