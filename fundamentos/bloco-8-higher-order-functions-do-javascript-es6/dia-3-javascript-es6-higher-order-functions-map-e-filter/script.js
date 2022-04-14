eb281cc33f8a59e8643e6678adeba1010ffa5222
Jenifergs <contato.comercialjenifer@gmail.com>
2022-04-13 22:12:29 -0300

    Questões 1, 2 e 3, bloco 8.3

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -0,0 +1,87 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  // Adicione o código do exercício aqui:
+
+  const formatedBookNames = (() => books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`))
+
+//   2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha, considerando suas idades quando o livro foi lançado.
+
+const nameAndAge = () => {
+  return  books.map((book) => {
+      return  {
+            author: book.author.name,
+            age: book.releaseYear - book.author.birthYear,
+        }
+    })
+    .sort((obj1, obj2) => obj1.age - obj2.age)}
+  
+    // 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+    const fantasyOrScienceFiction = () => {
+        return books.filter ((book) => {
+            book.genre === 'Fantasia' || book.genre === 'Ficção Científica'
+        })
+    }
+
+    
