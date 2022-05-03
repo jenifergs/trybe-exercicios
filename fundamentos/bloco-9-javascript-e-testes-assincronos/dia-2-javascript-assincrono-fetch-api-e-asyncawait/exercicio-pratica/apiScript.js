d92c7a3955c1c8d0a5df77667818941a2c81928a
jenifergs <contato.comercialjenifer@gmail.com>
2022-05-03 12:11:56 -0300

    exercicios

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicio-pratica/apiScript.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicio-pratica/apiScript.js
@@ -1,4 +1,6 @@
 // apiScript.js     
+
+//Recebemos um objeto, certo? A partir daí, faça a piada aparecer no DOM da sua página!
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
