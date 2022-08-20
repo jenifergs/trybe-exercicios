70041fb72c38d27d607ad4fa65bb1739cb833b90
jenifergs <contato.comercialjenifer@gmail.com>
2022-08-20 17:06:08 -0300

    feat: :sparkles: Hello world em nodejs juntamente com pacote readline 🤩

--- backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/hello-world/index.js
+++ backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/hello-world/index.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+const readline = require('readline-sync');
+
+const name = readline.question('Qual seu nome? ');
+const age = readline.questionInt('Qual sua idade? ');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
