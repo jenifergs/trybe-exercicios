f7ba53ec9647b0f1312f225c57231d155ae1044d
jenifergs <contato.comercialjenifer@gmail.com>
2022-08-21 12:37:13 -0300

    feat: :sparkles: Exercicio de calculo de imc interativo com readline-sync

--- backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/Exercicio-bloco-22-dia-1/bmi.js
+++ backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/Exercicio-bloco-22-dia-1/bmi.js
@@ -1,18 +1,59 @@
-const weightInKg = 80; 
-const heightInCm = 163;
+const readline = require('readline-sync');
 
 function handleBMI(weight, height) {
   console.log(`Weight: ${weight}, Height: ${height}`);
-  
+
   const bmi = weight / (height / 100) ** 2;
   
   return bmi;
 }
 
-function main() {
-  const bmi = handleBMI(weightInKg, heightInCm);
+const BMI_MAX_AND_MIN = {
+    'Underweight': {
+      minBMI: 0,
+      maxBMI: 18.4,
+    },
+    'Normal Weight': {
+      minBMI: 18.5,
+      maxBMI: 24.9,
+    },
+    'Overweight': {
+      minBMI: 25,
+      maxBMI: 29.9,
+    },
+    'Obese Class I': {
+      minBMI: 30.0,
+      maxBMI: 34.9,
+    },
+    'Obese Class II': {
+      minBMI: 35,
+      maxBMI: 39.9,
+    },
+    'Obese Class III': {
+      minBMI: 40,
+      maxBMI: 100,
+    },
+  };
 
-  console.log(`BMI: ${bmi.toFixed(2)}`);
+  function handleBMIResult(imc) {
+    const situacoes = Object.keys(BMI_MAX_AND_MIN);
+  
+    const resultFind = situacoes.find((situacao) => {
+      const { maxBMI, minBMI } = BMI_MAX_AND_MIN[situacao];
+  
+      return imc >= minBMI && imc <= maxBMI;
+    });
+  
+    return resultFind;
+  }
+
+function main() {
+    const weight = readline.questionFloat('What\'s your weight? (kg) ');
+    const height = readline.questionInt('What\'s your height? (cm) ');
+  const bmi = handleBMI(weight, height);
+  const bmiResult = handleBMIResult(bmi);
+
+  console.log(`bmiResult:${bmi.toFixed(2)} - ${bmiResult}`);
 }
 
 main();
