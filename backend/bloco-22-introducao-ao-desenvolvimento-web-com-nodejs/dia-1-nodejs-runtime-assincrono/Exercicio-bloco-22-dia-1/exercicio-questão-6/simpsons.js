e60e7bf2f4d4505b3db3b25b7b937659e71b5e22
jenifergs <contato.comercialjenifer@gmail.com>
2022-08-21 14:09:22 -0300

    feat: :sparkles: Adiciona requisito 2 do exercicio 6, 22.1

--- backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/Exercicio-bloco-22-dia-1/exercicio-questão-6/simpsons.js
+++ backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/Exercicio-bloco-22-dia-1/exercicio-questão-6/simpsons.js
@@ -9,6 +9,22 @@ async function readAll() {
 
 async function main() {
   await readAll();
+  const simpson = await getSimpsonById(1);
+//   const simpson2 = await getSimpsonById(15);
+  console.log(simpson);
 }
+
+async function getSimpsonById(id) {
+    const fileContent = await fs
+      .readFile('./simpsons.json', 'utf-8');
+    
+    const simpsons = JSON.parse(fileContent);
+    const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
+
+     if (!chosenSimpson) {
+       throw new Error('id não encontrado');
+     }
+     return chosenSimpson;
+  }
 
 main();
