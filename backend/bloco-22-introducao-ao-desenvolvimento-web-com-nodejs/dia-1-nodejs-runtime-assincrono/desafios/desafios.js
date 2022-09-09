b3c51b6a090a64f9c1c58d598bdfea45a6fb98b8
jenifergs <contato.comercialjenifer@gmail.com>
2022-09-09 11:04:12 -0300

    feat: :white_check_mark: Adiciona desafio 22.1 ordenar array sem sort

---
+++ backend/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/desafios/desafios.js
@@ -0,0 +1,36 @@
+// crie uma função que receba um array de numeros
+// e retorne um array ordenado de forma crescente
+function sort(array) {
+  return array.sort((a, b) => a - b);
+};
+
+console.log(sort([3, 5, 1, 9, 4, 2, 7]));
+
+
+function ordenacaoSimples(array) {
+    const copia = [...array] // cria um array de copia para nao modificar o original
+    const novoArray = [] // cria um novo array vazio para receber os valores ordenados
+    
+    while (novoArray.length < array.length) {
+    // pegar o menor item do array
+    // eslint-disable-next-line unicorn/no-array-reduce
+    const menorItem = copia.reduce((menor, item) => {
+    return menor < item ? menor : item
+    })
+    
+    // se fosse o maior item seria:
+    // const maiorItem = copia.reduce((maior, item) => {
+    // return maior > item ? maior : item
+    // })
+    
+    // adicionar o menor item ao novo array
+    novoArray.push(menorItem)
+    
+    // remover o menor item do array
+    copia.splice(copia.indexOf(menorItem), 1)
+    }
+    
+    return novoArray
+    }
+
+    console.log(ordenacaoSimples([3, 5, 1, 9, 4, 2, 7]));
