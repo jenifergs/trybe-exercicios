d4f7f63bab11ea9fdb17682be8267a592925403b
jenifergs <contato.comercialjenifer@gmail.com>
2022-10-18 23:58:23 -0300

    feat:

--- backend/secao-5/dia-1/migrations/20221019025345-books.js
+++ backend/secao-5/dia-1/migrations/20221019025345-books.js
@@ -3,12 +3,8 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    id: Sequelize.INTEGER;
+    title: Sequelize.STRING;
   },
 
   async down (queryInterface, Sequelize) {
