158177c2f536f8e4294ff5fcadf233f030240768
jenifergs <contato.comercialjenifer@gmail.com>
2022-10-19 00:00:21 -0300

    feat:

--- backend/secao-5/dia-1/migrations/20221019025345-books.js
+++ backend/secao-5/dia-1/migrations/20221019025345-books.js
@@ -9,11 +9,8 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
+    await queryInterface.dropTable('users');
+
   }
 };
