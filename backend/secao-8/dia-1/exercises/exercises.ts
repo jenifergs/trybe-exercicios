96477575c9b708e499b1380186a5d7da8b05321d
jenifergs <contato.comercialjenifer@gmail.com>
2022-12-21 17:05:45 -0300

    feat:

--- backend/secao-8/dia-1/exercises/exercises.ts
+++ backend/secao-8/dia-1/exercises/exercises.ts
@@ -7,3 +7,7 @@ export function getSquareArea(side: number): number {
 export function getRectangleArea(base: number, height: number): number {
   return base * height;
 }
+
+export function getTriangleArea(base: number, height: number): number {
+  return (base * height) / 2;
+}
