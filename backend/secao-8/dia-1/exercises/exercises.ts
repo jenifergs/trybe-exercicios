df663ff4f9ea8402a245035428277a66146f4522
jenifergs <contato.comercialjenifer@gmail.com>
2022-12-21 17:02:29 -0300

    feat: Revisao secao 8 introducao a ts

---
+++ backend/secao-8/dia-1/exercises/exercises.ts
@@ -0,0 +1,9 @@
+// ./exercises.ts
+
+export function getSquareArea(side: number): number {
+  return side ** 2;
+}
+
+export function getRectangleArea(base: number, height: number): number {
+  return base * height;
+}
