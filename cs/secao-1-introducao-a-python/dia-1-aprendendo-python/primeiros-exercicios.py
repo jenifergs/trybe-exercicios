c97e04729310b4934707714a51d1e68e23962e81
jenifergs <contato.comercialjenifer@gmail.com>
2023-02-13 18:01:39 -0300

    feat: Iniciando módulo cs, introdução a python (seção 1 - dia 1)

---
+++ cs/secao-1-introducao-a-python/dia-1-aprendendo-python/primeiros-exercicios.py
@@ -0,0 +1,29 @@
+a = 10
+b = 5
+a + b
+a - b
+a * b
+a / b
+a // b
+a ** b
+a % b
+
+print("Olá, mundo!")
+
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+print(minutes)
+print(seconds)
+
+# Exercício 4: Suponha que o preço de capa de um livro seja R$ 24,20,
+#  mas as livrarias recebem um desconto de 40%.
+#  O transporte custa 3,00 para o primeiro exemplar e 75 centavos
+#  para cada exemplar adicional. Qual é o custo total de atacado
+#  para 60 cópias? Escreva uma expressão que receba o custo total e a imprima.
+
+books = 60
+book_price = (1 - 0.4) * 24.20
+logistic = 3 + (books - 1) * 0.75
+cost = books * book_price + logistic
+print(cost)
