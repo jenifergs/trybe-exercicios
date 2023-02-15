310e8088b77b272014e37a0930a32b37090fc71f
jenifergs <contato.comercialjenifer@gmail.com>
2023-02-15 15:14:42 -0300

    feat: Exercicios 5 e 6 dia 1 introdução a python

--- cs/secao-1-introducao-a-python/dia-1-aprendendo-python/primeiros-exercicios.py
+++ cs/secao-1-introducao-a-python/dia-1-aprendendo-python/primeiros-exercicios.py
@@ -27,3 +27,14 @@ book_price = (1 - 0.4) * 24.20
 logistic = 3 + (books - 1) * 0.75
 cost = books * book_price + logistic
 print(cost)
+
+#Exercício 5: Adicione o elemento “Ciência da Computação” à lista.
+#Exercício 6: Acesse e altere o primeiro elemento da lista para “Fundamentos”.
+
+trybe_course = ["Introdução", "Front-end", "Back-end"]
+
+trybe_course.append("Ciência da Computação")
+
+trybe_course[0] = "Fundamentos"
+
+print(trybe_course)
